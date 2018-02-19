3ee18992ac70068f41ae2427bc17b629dabab4a2
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-02-19 03:15:41 -0600

    Estilizado e importacion de CSS para JSX

--- src/Persona/Persona.js
+++ src/Persona/Persona.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Persona.css'
 
 const persona = (props) => {
         // Children refiere a todos los elementos que se encuentren entre los tags 
         // de cualquier JSX (En este caso "Persona")
     return (
         // onChange se ejecuta cuando algo cambia dentro del elemento
-        <div>
+        <div className = "Persona">
             <h1 onClick={props.click}>Soy una persona de nombre {props.nombre} y tengo {props.edad} de edad!</h1>
             <p>{props.children}</p>
             <input type="text" onChange={props.cambio} value = {props.nombre} />
