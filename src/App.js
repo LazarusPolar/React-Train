18dc9fb6ff9f78d1a8dbbec443468f3f6b46b781
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-02-19 03:18:57 -0600

    Manera alternativa de estilizado con JSX

--- src/App.js
+++ src/App.js
@@ -57,6 +57,16 @@ class App extends Component {
   
 
   render() {
+
+    //Solucion alternativa de estilos CSS con Javascript 
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     // Se llama al parametro this y state para obtener los datos a ingresar 
     // Se pueden pasar handler de onClick a traves de las propiedades de parametros 
     // Es recomendable usar siempre el "bind()"
@@ -65,7 +75,7 @@ class App extends Component {
         <h1> Ejemplo React </h1>
         <p> Funcionamiento de parrafo </p>
 
-        <button onClick = {() => this.cambiaNombreHandler('Leopoldo')}>Cambiar nombre </button>
+        <button style={style} onClick = {() => this.cambiaNombreHandler('Leopoldo')}>Cambiar nombre </button>
         <Persona nombre = {this.state.personas[0].nombre} edad = {this.state.personas[0].edad}/>
 
         <Persona nombre = {this.state.personas[1].nombre} 
