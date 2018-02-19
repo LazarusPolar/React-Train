a75754f8d145aaa2a5d9b0256621723a527a09a5
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-02-19 01:33:40 -0600

    Se agrega ejemplo React.createElement()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// React se basa en crear componentes
 class App extends Component {
   render() {
-    return (
+    /*return (
       <div className="App">
         <h1> Ejemplo React </h1>
       </div>
-    );
+    );*/
+    
+    // Genera elementos. 
+    // Equivale al codigo JSX representado en la parte superior.
+    // El primer parametro es el componente que se desea generar, el segundo parametro es la configuracion y, el tercero, es un elemento extra
+    // anidado al elemento principal <div>
+   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Aplicacion React!"));
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Se renderiza la vista de App.js
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
