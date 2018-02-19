6347b189ec33ce7bf04ca8545c7811c8b3e08088
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-02-19 02:31:06 -0600

    Agrega ejemplo de props y states

--- src/App.js
+++ src/App.js
@@ -4,13 +4,52 @@ import Persona from './Persona/Persona'
 
 // React se basa en crear componentes
 class App extends Component {
+  // state solo esta disponible al extender de la clase "Component"
+  state = {
+    personas: [
+      {
+        nombre: 'Jose', edad: 23
+      },
+      {
+        nombre: 'Alberto', edad: 28
+      },
+      {
+        nombre: 'Diana', edad: 19
+      }
+    
+    ]
+  }
+
+  cambiaNombreHandler = () => {
+    console.log("Hola")
+    // ESTO NO SE DEBE HACER
+    /// this.state.personas[0].nombre = 'Alejandro'
+
+    // Asi se actualizan la info. en el parametro "state"
+    this.setState({personas: [
+      {
+        nombre: 'Alejandro', edad: 22
+      },
+      {
+        nombre: 'Rodrigo', edad: 34
+      },
+      {
+        nombre: 'Borgia', edad: 31
+      }
+    
+    ]})
+  }
+
   render() {
+    // Se llama al parametro this y state para obtener los datos a ingresar 
     return (
       <div className="App">
         <h1> Ejemplo React </h1>
         <p> Funcionamiento de parrafo </p>
-        <Persona nombre = "Jose" edad = "23"/>
-        <Persona nombre = "Alberto" edad = "25" >Tengo informacion adicional</Persona>
+        <button onClick = {this.cambiaNombreHandler}>Cambiar nombre </button>
+        <Persona nombre = {this.state.personas[0].nombre} edad = {this.state.personas[0].edad}/>
+        <Persona nombre = {this.state.personas[1].nombre} edad = {this.state.personas[1].edad} > Tengo informacion adicional </Persona>
+        <Persona nombre = {this.state.personas[2].nombre} edad = {this.state.personas[2].edad}/>
       </div>
     );
 
--- src/Persona/Persona.js
+++ src/Persona/Persona.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const persona = (props) => {
-    return <h1>Soy una persona de nombre {props.nombre} y tengo {props.edad} de edad!</h1>
+        // Children refiere a todos los elementos que se encuentren entre los tags 
+        // de cualquier JSX (En este caso "Persona")
+    return (
+        <div>
+            <h1>Soy una persona de nombre {props.nombre} y tengo {props.edad} de edad!</h1>
+            <p>{props.children}</p>
+        </div>)
 }
 
 export default persona;
