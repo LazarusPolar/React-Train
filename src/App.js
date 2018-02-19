b80bec85cffc9d9b24075770e397d25c04236d8a
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-02-19 03:09:31 -0600

    Agrega ejemplo de 'event' para carga de contenido dinamico con entrada de usuario

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     ]
   }
 
-  cambiaNombreHandler = () => {
+  cambiaNombreHandler = (nuevoNombre) => {
     console.log("Hola")
     // ESTO NO SE DEBE HACER
     /// this.state.personas[0].nombre = 'Alejandro'
@@ -31,7 +31,7 @@ class App extends Component {
         nombre: 'Alejandro', edad: 22
       },
       {
-        nombre: 'Rodrigo', edad: 34
+        nombre: nuevoNombre, edad: 34
       },
       {
         nombre: 'Borgia', edad: 31
@@ -40,15 +40,41 @@ class App extends Component {
     ]})
   }
 
+  // "Event" se encarga de revisar que el componente se este utilizando 
+  nombreCambiadoHandler = (event) => {
+    this.setState({
+    personas: [
+      {
+        nombre: 'Alejandro', edad: 22
+      },
+      {
+        nombre: event.target.value, edad: 34
+      },
+      {
+        nombre: 'Borgia', edad: 31
+      }
+    ]})}
+  
+
   render() {
     // Se llama al parametro this y state para obtener los datos a ingresar 
+    // Se pueden pasar handler de onClick a traves de las propiedades de parametros 
+    // Es recomendable usar siempre el "bind()"
     return (
       <div className="App">
         <h1> Ejemplo React </h1>
         <p> Funcionamiento de parrafo </p>
-        <button onClick = {this.cambiaNombreHandler}>Cambiar nombre </button>
+
+        <button onClick = {() => this.cambiaNombreHandler('Leopoldo')}>Cambiar nombre </button>
         <Persona nombre = {this.state.personas[0].nombre} edad = {this.state.personas[0].edad}/>
-        <Persona nombre = {this.state.personas[1].nombre} edad = {this.state.personas[1].edad} > Tengo informacion adicional </Persona>
+
+        <Persona nombre = {this.state.personas[1].nombre} 
+        edad = {this.state.personas[1].edad} 
+        click={this.cambiaNombreHandler.bind(this, "Marcos")} 
+        cambio = {this.nombreCambiadoHandler}> 
+        Tengo informacion adicional 
+        </Persona>
+
         <Persona nombre = {this.state.personas[2].nombre} edad = {this.state.personas[2].edad}/>
       </div>
     );
--- src/Persona/Persona.js
+++ src/Persona/Persona.js
@@ -4,9 +4,11 @@ const persona = (props) => {
         // Children refiere a todos los elementos que se encuentren entre los tags 
         // de cualquier JSX (En este caso "Persona")
     return (
+        // onChange se ejecuta cuando algo cambia dentro del elemento
         <div>
-            <h1>Soy una persona de nombre {props.nombre} y tengo {props.edad} de edad!</h1>
+            <h1 onClick={props.click}>Soy una persona de nombre {props.nombre} y tengo {props.edad} de edad!</h1>
             <p>{props.children}</p>
+            <input type="text" onChange={props.cambio} value = {props.nombre} />
         </div>)
 }
 
